748676d338747c816211ef8e2ab3b67c4f5275c7
Dustin Martens <dmartens.it@gmail.com>
2019-04-14 20:44:08 -0500

    Added CSS to Create Account page
    Duplicated the look of the login page to create a consistent look across login pages

--- create-account.php
+++ create-account.php
@@ -8,35 +8,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Create Account</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="login.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-    <div>
-        <form action="submit-account.php" method="post" enctype="multipart/form-data">
-            <p>
-                <label for="username">Username:</label>
-                <input id="username" name ="username" type="text" />
-            </p>
-            <p>
-                <label for="email">Email:</label>
-                <input id="email" name="email" type="email" />
-            </p>
-            <p>
-                <label for="password">Password:</label>
-                <input id="password" name="password" type="password" />
-            </p>
-            <p>
-                <label for="confirm-password">Confirm password:</label>
-                <input id="confirm-password" name="confirm-password" type="password" />
-            </p>
-            <p>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <form action="submit-account.php" method="post" enctype="multipart/form-data">
+                <input type="text" id="username" name="username" placeholder="username" />
+                <input type="email" id="email" name="email" placeholder="email" />
+                <input type="password" id="password" name="password" placeholder="password" />
+                <input type="password" id="confirm-password" name="confirm-password" placeholder="confirm password" />
                 <label for="profile-image">Profile picture:</label>
                 <input type="file" name="profile-image" id="profile-image">
-            </p>
-            <p>
                 <input type="submit" name="create" value="Create Account" />
-            </p>
-        </form>
+            </form>
+        </div>
     </div>
 </body>
 </html>
